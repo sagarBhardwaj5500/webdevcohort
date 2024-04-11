7942a0fe09824184009794b2034c895b172398ee
sagar bhardwaj <bharsagar5@gmail.com>
2024-04-11 23:10:56 +0530

    Add files via upload

---
+++ anagram.js
@@ -0,0 +1,23 @@
+/*
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  What's Anagram?
+  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+
+*/
+
+const str1="listen"
+const str2="silent"
+
+function isAnagram(str1, str2) {
+
+let n1=str1.length;
+let n2 =str2.length;
+
+
+}
+
+
+const value = isAnagram(str1,str2)
+
+module.exports = isAnagram;
+
